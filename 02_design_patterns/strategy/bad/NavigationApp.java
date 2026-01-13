ebf041777107796cd62eb09606dd879bfd1fd1a8
kuxha <labakumardeka42@gmail.com>
2026-01-12 22:58:24 -0500

    added strategy pattern

---
+++ 02_design_patterns/strategy/bad/NavigationApp.java
@@ -0,0 +1,14 @@
+
+public class NavigationApp {
+    public void buildRoute(String transportType) {
+        if (transportType.equals("car")) {
+            System.out.println("Building route for Car: 15 mins");
+        } else if (transportType.equals("bike")) {
+            System.out.println("Building route for Bike: 25 mins");
+        } else if (transportType.equals("walk")) {
+            System.out.println("Building route for Walk: 45 mins");
+        } else {
+            System.out.println("Unknown transport type");
+        }
+    }
+}
